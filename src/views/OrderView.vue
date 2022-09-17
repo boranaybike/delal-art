01e6c3f5b9ef1eb33f533dc403ac5849cef22e4d
ik3 <aybikeboran7140@gmail.com>
2022-09-17 21:50:37 +0300

    Update Order Page

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="order">
-    <div class="title">Sipariş için:</div>
-    <div id="subtitle">
-      <div><div>mail adresim üzerinden benimle iletişime geçebilirsiniz</div></div>
-      <div><div>instagram adresim üzerinden benimle iletişime geçebilirsiniz</div></div>
-      <div><div>shoppier hesabımdan sipariş verebilirsiniz</div></div>
-    </div>
+    <v-row>
+      <v-col cols="6">
+        <div class="container">
+          <div class="title">Siparişleriniz için,</div>
+          <div id="subtitle">
+            <div>
+              <v-icon color="#f77f53">mdi-email-outline</v-icon> mail adresim üzerinden benimle iletişime
+              geçebilirsiniz:
+            </div>
+            <div>
+              <v-icon color="#f77f53">mdi-instagram</v-icon> instagram adresim üzerinden benimle iletişime
+              geçebilirsiniz: @delaleylem.art
+            </div>
+            <div><v-icon color="#f77f53">mdi-shopping</v-icon>shoppier hesabımdan sipariş verebilirsiniz:</div>
+          </div>
+        </div>
+      </v-col>
+
+      <v-col cols="6">
+        <v-img contain src="@/assets/eyes0.jpg"> </v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -22,18 +38,27 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.container {
+  width: 30%;
+  z-index: 2;
+  position: absolute;
+  left: 30%;
+  top: 20%;
+  background-color: white;
+}
 .title {
   padding: 5%;
-  font-size: 50px !important;
-  color: #f5c722;
+  font-size: 30px !important;
+  color: #f55b22;
 }
-#subtitle > div > div {
+#subtitle > div {
   text-align: left;
-  font-size: 30px;
+  font-size: 20px;
   color: #f77f53;
+  padding: 1% 0;
 }
 .order {
-  padding: 10% 20% 5% 20%;
+  padding: 15% 15% 5% 15%;
 }
 .col {
   padding: 1%;
