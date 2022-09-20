94e38246ad62eb4d35062aa2915527f5bacde694
ik3 <aybikeboran7140@gmail.com>
2022-09-20 19:37:21 +0300

    Update OnSale

--- src/views/OnSaleView.vue
+++ src/views/OnSaleView.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="on-sale">
     <v-row>
-      <v-col class="d-flex child-flex" cols="4">
-        <v-img contain src="@/assets/s11.jpg"> </v-img>
+      <v-col cols="12" sm="4">
+        <a href="https://www.shopier.com/ShowProductNew/products.php?id=13409602">
+          <v-img contain src="@/assets/s31.jpg"> </v-img
+        ></a>
       </v-col>
-      <v-col class="d-flex child-flex" cols="4">
+      <v-col cols="12" sm="4">
         <v-img contain src="@/assets/s2.jpg"> </v-img>
       </v-col>
-      <v-col class="d-flex child-flex" cols="4">
-        <v-img contain src="@/assets/s31.jpg"> </v-img>
+      <v-col cols="12" sm="4">
+        <a href="https://www.shopier.com/ShowProductNew/products.php?id=13409520">
+          <v-img contain src="@/assets/1.jpg"> </v-img
+        ></a>
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="d-flex child-flex" cols="4">
-        <v-img contain src="@/assets/s41.jpg"> </v-img>
+      <v-col cols="12" sm="4">
+        <v-img contain src="@/assets/s11.jpg"> </v-img>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <a href="https://www.shopier.com/ShowProductNew/products.php?id=13409380">
+          <v-img contain src="@/assets/s41.jpg"> </v-img
+        ></a>
       </v-col>
     </v-row>
   </div>
@@ -25,5 +34,13 @@
 }
 .col {
   padding: 1%;
+}
+.v-img.on-hover {
+  background-color: rgba(#fff, 0.8);
+}
+@media screen and (max-width: 800px) {
+  .on-sale {
+    padding: 30% 20% 5% 20%;
+  }
 }
 </style>
